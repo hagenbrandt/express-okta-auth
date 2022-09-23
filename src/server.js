dd43178be5172bb02b86c56b5f9b1f73dceaea2d
hbrandt <h.brandt@steinberg.de>
2022-09-23 14:28:53 +0200

    refactor: Adjust post method to post given data body

--- src/server.js
+++ src/server.js
@@ -7,13 +7,12 @@ const port = 3000
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.send({message: 'hello'})
+app.get('/data', (req, res) => {
+  res.send({ message: 'hello' })
 })
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-    res.send({message: 'ok'})
+app.post('/data', (req, res) => {
+  res.send(req.body)
 })
 
 export const start = () => {
