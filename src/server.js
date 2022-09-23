b8e0a2ae63a1ecd31585cf4aecde90cd41c6efea
hbrandt <h.brandt@steinberg.de>
2022-09-23 14:36:43 +0200

    build: Install cors and morgan

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
 
 app.get('/data', (req, res) => {
   res.send({ message: 'hello' })
