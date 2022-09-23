caab096b592dbb9b18cea608b7104c3683ec6694
hbrandt <h.brandt@steinberg.de>
2022-09-23 15:50:50 +0200

    test: Create test for routes

--- src/resources/item/__tests__/item.router.spec.js
+++ src/resources/item/__tests__/item.router.spec.js
@@ -1,5 +1,20 @@
-describe('item.router', () => {
-    it('renders all routes', () => {
-        console.log('Test');
+import router from '../item.router'
+
+describe('item router', () => {
+    it('has crud routes', () => {
+        const routes = [
+            { path: '/', method: 'get' },
+            { path: '/:id', method: 'get' },
+            { path: '/:id', method: 'delete' },
+            { path: '/:id', method: 'put' },
+            { path: '/', method: 'post' },
+        ]
+
+        routes.forEach(route => {
+            const match = router.stack.find(
+                s => s.route.path === route.path && s.route.methods
+            )
+            expect(match).toBeTruthy()
+        })
     })
 })
