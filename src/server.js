4c017eb4ddd0ef1288d4112e560446577448b345
hbrandt <h.brandt@steinberg.de>
2022-09-23 15:02:15 +0200

    feat: Initialize express router

--- src/server.js
+++ src/server.js
@@ -4,12 +4,19 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 const app = express()
+const router = express.Router()
 const port = 3000
 
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.use('/api', router)
 
 app.get('/data', (req, res) => {
   res.send({ message: 'hello' })
