215c3cee8ae35bf980017173a1ed31a305e3b5df
hbrandt <h.brandt@steinberg.de>
2022-10-06 10:51:36 +0200

    feat: Create basic controller for item

---
+++ src/resources/item/item.controller.js
@@ -0,0 +1,14 @@
+import { Item } from './item.modal'
+import mongoose from 'mongoose'
+
+const run = async () => {
+    const item = await Item.create({
+        name: 'Test-Name',
+        createdBy: mongoose.Types.ObjectId(),
+        list: mongoose.Types.ObjectId()
+    })
+
+    console.log(item);
+}
+
+run()
