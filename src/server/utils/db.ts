3d2ca612aedde548d774193bef523cdfc649f6db
hbrandt <h.brandt@steinberg.de>
2022-10-17 10:50:40 +0200

    refactor: Change url for mongodb

--- src/server/utils/db.ts
+++ src/server/utils/db.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const uri = process.env.DATABASE_URL ?? 'mongodb://localhost:27017'
+const uri = process.env.MONGODB_CLOUD ?? 'mongodb://localhost:27017'
 
 export const connect = () => {
     return mongoose.connect(uri)
