4151cc4a2b7768f077a08ca0d7433c555e46c41e
hbrandt <h.brandt@steinberg.de>
2022-10-17 16:58:39 +0200

    feat: Push data from frontende to database

--- src/client/components/app.tsx
+++ src/client/components/app.tsx
@@ -1,3 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const App = () => <h1>Hello Frontend</h1>
+export const App = () => {
+    const [data, setData] = useState(
+        {
+            "title": "Very new title",
+            "ingredients": {
+                "oneCup": "something"
+            },
+            "description": ["a description text"]
+        }
+    )
+
+return (
+    <>
+    <h1>Hello Frontend</h1>
+    <button onClick={postDataToDB}>Set data to database</button>
+    </>
+)
+
+async function postDataToDB() {
+    if (!data) {
+        console.log('No data');
+        return
+    }
+    console.log('data', data);
+    
+    await fetch('http://localhost:8080/api/recipes/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then((res) => res.json()).then((data) => {
+        console.log('Success:', data);
+    }).catch(console.error)
+}
+}
