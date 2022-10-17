ffdecfae19b54d0a2e084021b6aa20b8dde0edd8
hbrandt <h.brandt@steinberg.de>
2022-10-17 15:33:34 +0200

    feat: Add template as initial html for server and seve react app in express

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,29 +1,16 @@
 // import express from 'express'
+import express, { Express } from 'express'
 import path from 'path'
+import fs from 'fs'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
-// import cors from 'cors'
-// import itemRouter from './resources/item/item.router'
-// import { connect } from './utils/db'
-
-// const app = express()
-// const port = 3000
-
-// app.use('/api/item', itemRouter)
-
-// export const start = () => {
-//   connect().then(async () => {
-//     app.listen(port, () => {
-//       console.log(`Example app listening on port ${port}`)
-//     })
-//   })
-// }
-
-import express, { Express } from "express"
-import cors from "cors"
+import cors from 'cors'
 require('dotenv').config()
 import recipeRoutes from './resources/recipe/recipe.router'
-import { connect } from "./utils/db"
+import { connect } from './utils/db'
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { App } from '../client/components/app'
 
 const app: Express = express()
 
@@ -35,19 +22,32 @@ app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use('/api/recipes', recipeRoutes)
 
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'))
+
+app.use('/', express.static(path.join(__dirname, 'static')))
+
+const manifest = fs.readFileSync(
+  path.join(__dirname, 'static/manifest.json'),
+  'utf-8'
+)
+const assets = JSON.parse(manifest)
+
+app.get('/', (req, res) => {
+  const component = ReactDOMServer.renderToString(React.createElement(App))
+  res.render('client', { assets, component })
 })
 
 export const start = () => {
   connect()
     .then(() =>
       app.listen(PORT, () =>
-        console.log(`Server running on http://localhost:${PORT}`, process.env.DATABASE_URL)
-        
+        console.log(
+          `Server running on http://localhost:${PORT}`
+        )
       )
     )
-    .catch(error => {
+    .catch((error) => {
       throw error
     })
 }
