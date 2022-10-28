e03633396fa5107b2e3e0bee2c30a79be00aa695
hbrandt <h.brandt@steinberg.de>
2022-10-28 16:38:12 +0200

    build: Install okta and setup basic settings for login

--- src/server/server.ts
+++ src/server/server.ts
@@ -22,6 +22,31 @@ app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use('/api/recipes', recipeRoutes)
 
+app.use(
+  require('express-session')({
+    secret: process.env.APP_SECRET,
+    resave: true,
+    saveUninitialized: false
+  })
+)
+
+const { ExpressOIDC } = require('@okta/oidc-middleware')
+const oidc = new ExpressOIDC({
+  appBaseUrl: process.env.HOST_URL,
+  issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  redirect_uri: `${process.env.HOST_URL}/callback`,
+  scope: 'openid profile',
+  routes: {
+    loginCallback: {
+      path: '/callback'
+    }
+  }
+})
+
+app.use(oidc.router)
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'))
 
