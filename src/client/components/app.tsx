a6a731163bac29cfdfcf1295317e48389f45969c
hbrandt <h.brandt@steinberg.de>
2022-12-16 14:51:15 +0100

    feat: Get protected api route in frontend app

--- src/client/components/app.tsx
+++ src/client/components/app.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { getUser } from '../helper/helperFunctions'
 import { AppRouter } from '../router/router'
 
 export const App = () => {
+  const isClient = typeof window !== 'undefined' && !!window.document
+  const jwtToken = getJwtFromCookie()
+  
   return (
     <>
       <h1 className="text-3xl font-bold">Recipe collector</h1>
+      <button onClick={() => getUser(jwtToken ?? '')}>Get User Data</button>
       <AppRouter />
     </>
   )
+
+  function getJwtFromCookie() {
+    if (isClient) {
+      return window.document.cookie.split('; ').find(item => item.includes('jwtToken'))?.split('=')[1]
+    }
+  }
 }
