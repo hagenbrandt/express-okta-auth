4a0b3828f9fce1531fa16950e33a0c97ce91a0ab
hbrandt <h.brandt@steinberg.de>
2022-12-16 15:30:49 +0100

    feat: Create user and document store

---
+++ src/client/store/documentStore.ts
@@ -0,0 +1,11 @@
+import create from 'zustand'
+
+type WindowDocument = {
+    isClient: boolean,
+    setIsClient: (isClient: boolean) => void
+}
+
+export const useDocumentStore = create<WindowDocument>((set) => ({
+    isClient: false,
+    setIsClient: (isClient: boolean) => set({ isClient: isClient })
+}))
