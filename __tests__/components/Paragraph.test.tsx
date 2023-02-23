95d7ee171fdacb47f868e207894c80b92cc10520
hbrandt <h.brandt@steinberg.de>
2023-02-23 09:39:11 +0100

    feat: Render a paragraph

--- __tests__/components/Paragraph.test.tsx
+++ __tests__/components/Paragraph.test.tsx
@@ -1,5 +1,17 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Paragraph from '../../src/client/components/Paragraph'
+
 describe('Paragraph', () => {
-  it.todo('returns a paragraph with given text')
+  const text = 'Example text'
+
+  it('returns a paragraph with given text', () => {
+    render(<Paragraph text={text} />)
+
+    expect(screen.getByText(text)).toBeInTheDocument()
+  })
+
   it.todo('returns empty container when no text is given')
   it.todo('has given className')
   it.todo('has no className when no className is given')
---
+++ src/client/components/Paragraph.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type ParagraphProps = {
+  text: string
+}
+
+const Paragraph = ({ text }: ParagraphProps) => <p>{text}</p>
+
+export default Paragraph
