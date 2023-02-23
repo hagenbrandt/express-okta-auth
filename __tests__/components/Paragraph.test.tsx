a2dcdc9521357a5a4e67ebafd7e092a5a94ca858
hbrandt <h.brandt@steinberg.de>
2023-02-23 09:31:32 +0100

    test: Create test todos for Paragraph

---
+++ __tests__/components/Paragraph.test.tsx
@@ -0,0 +1,6 @@
+describe('Paragraph', () => {
+  it.todo('returns a paragraph with given text')
+  it.todo('returns empty container when no text is given')
+  it.todo('has given className')
+  it.todo('has no className when no className is given')
+})
